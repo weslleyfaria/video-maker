cc28b97a5cea9095409cb50911637c223798d5fb
weslleygfaria <weslleygfaria@gmail.com>
2021-05-16 16:52:53 -0300

    feat: start text robot

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const readline = require('readline-sync')
+const robots = {
+    text: require('./robots/text.js')
+}
 
-function start() {
+async function start() {
     const content = {}
 
     content.searchTerm = askAndReturnSearch()
     content.prefix = askAndReturnPrefix()
+
+    await robots.text(content)
     
     function askAndReturnSearch() {      
         return readline.question('Type a Wikipedia search term: ')
@@ -15,10 +20,10 @@ function start() {
         const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
         const selectedPrefixText = prefixes[selectedPrefixIndex]
 
-        return askAndReturnPrefix
+        return selectedPrefixText
     }
 
-    console.log(content)
+    // console.log(content)
 }
 
 start()
